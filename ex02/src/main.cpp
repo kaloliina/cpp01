f35c6825c5298e53efde40377061dd1c54089247
Karoliina Hiidenheimo <khiidenh@c2r1p8.hive.fi>
2025-09-04 10:52:03 +0300

    Fixed one bug with the address exercise

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,7 +7,7 @@ int main(void)
 	std::string& stringREF = str;
 
 	std::cout << std::addressof(str) << std::endl;
-	std::cout << std::addressof(stringPTR) << std::endl;
+	std::cout << stringPTR << std::endl;
 	std::cout << std::addressof(stringREF) << std::endl;
 	std::cout << str << std::endl;
 	std::cout << *stringPTR << std::endl;
